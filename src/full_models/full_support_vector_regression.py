56303224c853567b3bd61cf0fc851dbb4aec53e6
ashtonckj <stickmangamer0730@gmail.com>
2026-08-03 23:59:02 +0800

    fix: funny svr graph

--- src/full_models/full_support_vector_regression.py
+++ src/full_models/full_support_vector_regression.py
@@ -18,8 +18,8 @@ X_train, X_test, y_train, y_test = load_full_split()
 FINAL_PARAMS = {
     "kernel": "rbf",
     "C": 100,
-    "epsilon": 5.0,
-    "gamma": 2.0
+    "epsilon": 1.0,
+    "gamma": 3.0
 }
 
 final_model = SVR(**FINAL_PARAMS)
